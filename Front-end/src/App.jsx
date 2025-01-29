1145a4b1bdc52fef41a82904ad482daef8790597
Mohammad Hasan <mohammadhasan707053@gmail.com>
2025-01-29 12:44:38 +0530

    milestone 9

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import {BrowserRouter , Routes , Route,Router } from 'react-router-dom';
-import { LoginPage } from './routes/Routes';
+import {BrowserRouter , Routes , Route  } from 'react-router-dom';
+import { CreateProduct, LoginPage } from './routes/Routes';
 import { SignupPage } from './routes/Routes';
+import { HomePage } from './routes/Routes';
 import  "./App.css"
 
 function App(){
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<HomePage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/signup" element={<SignupPage/>} />
+    <Route path = "/create-Product" element = {<CreateProduct/>} /> 
     </Routes>
     </BrowserRouter>
   );
--- Front-end/src/routes/Routes.js
+++ Front-end/src/routes/Routes.js
@@ -2,5 +2,7 @@ import LoginPage from "../pages/LoginPage.jsx";
 import Login from "../components/Auth/Login.jsx";
 import Signup from "../components/Auth/Signup.jsx"
 import SignupPage from "../pages/SignupPage.jsx";
+import HomePage from "../pages/HomePage.jsx";
+import CreateProduct from "../pages/createProduct.jsx";
 
-export {Login,LoginPage,Signup,SignupPage};
+export {Login,LoginPage,Signup,SignupPage,HomePage,CreateProduct};
